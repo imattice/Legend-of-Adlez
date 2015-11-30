2cefcbd96a9ce85e45fbc8d08445b8264f4ce0ea
Ike Mattice <student@epicodus.com>
2015-11-30 13:24:36 -0800

    bug fix?

--- js/Game.js
+++ js/Game.js
@@ -27,7 +27,7 @@ TopDownGame.Game.prototype = {
             this.game.physics.arcade.enable(this.player);
             this.player.health = 10;
             for (i = 1; i <= 10; i++) {
-                $('#game-frame').append('<img src="/assets/images/heart.png" height="72em" width="72em">');
+                $('#game-frame').append('<img src="/assets/images/heart.PNG" height="72em" width="72em">');
             }
 
         this.blockedLayer = this.map.createLayer('CANTGOHERE');
